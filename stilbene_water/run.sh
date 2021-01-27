c4e1add169ee07c2ea2320d487995c31134422c5
Dmitry Morozov <aracsmd@gmail.com>
2021-01-27 10:50:20 +0200

    Tutorial files

---
+++ stilbene_water/run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH -J stilbene_wat
+#SBATCH -t 00:30:00
+#SBATCH --ntasks-per-node=32
+#SBATCH --cpus-per-task=4
+#SBATCH --nodes=1
+#SBATCH --partition=test
+#SBATCH --account=project_
+#SBATCH --reservation=gromacs
+
+module purge
+module load cp2k/8.1-gmx
+
+export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
+export OMP_PLACES=cores
+
+srun gmx_cp2k mdrun -npme 0 -s stilbene-sol-opt.tpr 
